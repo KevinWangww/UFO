4151c0c36e6ca052acc8797b9a9cf66f4485ebb9
KevinWangww <wangziqi47369@gmail.com>
2025-01-21 16:29:51 -0500

    121

--- Assets/scripts/Cow.cs
+++ Assets/scripts/Cow.cs
@@ -8,31 +8,31 @@ public class Cow : MonoBehaviour
     [Header("动画曲线 (0~1 对应升/降的进度)")]
     public AnimationCurve moveCurve;
 
-    // 创建初始位置和最终位置
+    // Creating original and end positions
     public Vector2 originalPos;
     Vector2 endPos;
-    // 插值用的变量 t，0表示原位置，1表示完全上升
+    // Lerp uses 0 for the original position and 1 for end
     private float t = 0f;
     float targetT = 0f;
 
     void Start()
     {
-        //设置最终位置为起始位置的y轴加3
+        //Setting end position equal original position + 3
         endPos = originalPos;
         endPos.y += 3;
     }
 
     void Update()
     {
-        // 1. 获取鼠标在屏幕上的位置，然后转换到世界坐标
+        // Get the position of the mouse on the screen and switch into world position
         Vector2 mousePos = Input.mousePosition;
         Vector2 worldPos = Camera.main.ScreenToWorldPoint(mousePos);
 
-        // 2. 计算鼠标与物体 X 坐标之间的绝对距离
+        // Get disition of x position between mouse and caw
         float distanceX = worldPos.x - transform.position.x;
 
-        // 3. 若距离小于 Range，则升起 (targetT=1)，否则回到原位置 (targetT=0)1
-        if((distanceX < 2 && distanceX >0) || (distanceX < 0 && distanceX > -2))
+        // If the distance is less than Range, raise (targetT=1), otherwise back to original position (targetT=0)
+        if ((distanceX < 2 && distanceX >0) || (distanceX < 0 && distanceX > -2))
         {
             targetT = 1f;
         }
@@ -41,10 +41,10 @@ public class Cow : MonoBehaviour
             targetT = 0f;
         }
         
-        // 4. 让 t 朝 targetT 平滑过渡
+        // Making t changes smoothly
         t = Mathf.Lerp(t, targetT, 3 * Time.deltaTime);
 
-        // 5. 用动画曲线来计算当前升/降的进度 (curveValue)
+        // Use AnimationCurves to calculate the current movement progress with t
         transform.position = Vector2.Lerp(originalPos, endPos, moveCurve.Evaluate(t));
         transform.Rotate(0f, 0f, 30 * t * Time.deltaTime);
 
